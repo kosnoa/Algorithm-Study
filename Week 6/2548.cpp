92882b3aa98afa06c5d9e88b8b52dc185338f22e
kosnoa <74532812+kosnoa@users.noreply.github.com>
2022-07-12 15:08:54 +0900

    Update 2548.cpp
    on progress

--- Week 6/2548.cpp
+++ Week 6/2548.cpp
@@ -7,9 +7,34 @@ typedef unsigned long long llu;
 
 vector<int> v;
 
-void BinarySearch(int len)
+void BinarySearch(int len, int target)
 {
-    
+    int low = (target + v[0]) / 2;
+    int high = (target + v[v.size() - 1]) / 2;
+    int mid;
+
+    while (low <= high)
+    {
+        int low_tmp = 0, mid_tmp = 0, high_tmp = 0;
+        mid = (low + high) / 2;
+
+        for (int i=0; i<v.size(); i++)
+        {
+            low_tmp += abs(low-v[i]);
+            mid_tmp += abs(mid-v[i]);
+            high_tmp += abs(high-v[i]);
+        }
+
+        if (mid_tmp < low_tmp && mid_tmp < high_tmp)
+        {
+            cout << mid << '\n';
+            break;
+        }
+        else if (mid_tmp > low_tmp && high_tmp > low_tmp)
+        {
+            mid = low;
+        }
+    }
 }
 
 int main()
@@ -18,8 +43,8 @@ int main()
 
     int n;
     cin >> n;
-    
-    for (int i=0; i<n; i++)
+
+    for (int i = 0; i < n; i++)
     {
         int tmp;
         cin >> tmp;
@@ -30,8 +55,15 @@ int main()
 
     // 첫 번쨰 경우에만 내가 해준다 (평균값 구하기 & 최소값과 평균값의 중간값 & 최댓값과 평균값의 중간값)
 
-    BinarySearch(v.size());
-   
+    int avg = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        avg += v[i];
+    }
+
+    avg /= v.size();
+
+    BinarySearch(v.size(), avg);
 
     return 0;
 }
