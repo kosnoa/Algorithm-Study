7702c7711f6b3ba5b4e27312fec18ea8c18a3db5
kosnoa <f4less0152@gmail.com>
2023-01-13 19:59:37 -0500

    update

---
+++ Others/11333.cpp
@@ -0,0 +1,45 @@
+/* Code By kosnoa
+GitHub: kosnoa */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+ll dp[10001];
+ll mod = 1000000007;
+
+// 20% 틀렸습니다
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        if (n % 3 != 0)
+        {
+            cout << "0" << '\n';
+            return 0;
+        }
+
+        dp[0] = 1;
+        dp[3] = 3;
+        dp[6] = 13;
+
+        for (int i = 9; i <= n; i += 3)
+        {
+            dp[i] = (((5 * dp[i - 3]) % mod) + mod - ((3 * dp[i - 6]) % mod) + dp[i - 9]) % mod;
+        }
+        cout << dp[n] << '\n';
+
+        memset(dp, 0, sizeof(dp));
+    }
+
+    return 0;
+}
