f8c0ec0af178af3f49528fbe42dd40d491378b8a
kosnoa <f4less0152@gmail.com>
2023-03-03 22:04:13 -0500

    work in progress

---
+++ Week 28/1162.cc
@@ -0,0 +1,79 @@
+/* Code By kosnoa
+GitHub : kosnoa */
+
+#pragma GCC optimize("Ofast")
+#pragma GCC("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+int n, m, k;
+vector<pair<ll, ll>> graph[10001];
+ll d[10005][25];
+
+/*
+3 3 1
+1 2 10
+1 3 1
+2 3 1
+
+답 0
+출력 1
+*/
+
+void dijkstra()
+{
+    priority_queue<tuple<ll, ll, ll>> pq;
+    pq.push({0, 1, 0});
+    d[1][0] = 0;
+
+    while (!pq.empty())
+    {
+        ll dist = -get<0>(pq.top());
+        int cur = get<1>(pq.top());
+        int cnt = get<2>(pq.top());
+        pq.pop();
+
+        for (int i = 0; i < graph[cur].size(); i++)
+        {
+            ll cost = dist + graph[cur][i].second;
+
+            if (cost < d[graph[cur][i].first][cnt])
+            {
+                d[graph[cur][i].first][cnt] = cost;
+                cout << "A. " << graph[cur][i].first << " " << cost << ' ' << cnt << '\n';
+                pq.push({-cost, graph[cur][i].first, cnt});
+            }
+
+            if (dist < d[graph[cur][i].first][cnt] && cnt < k)
+            {
+                int tmp = cnt + 1;
+                d[graph[cur][i].first][cnt] = dist;
+                cout << "B. " << graph[cur][i].first << " " << dist << ' ' << tmp << '\n';
+                pq.push({-dist, graph[cur][i].first, tmp});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
+    }
+
+    fill(&d[0][0], &d[10004][25], 1e9);
+
+    dijkstra();
+
+    cout << d[n][k] << '\n';
+
+    return 0;
+}
